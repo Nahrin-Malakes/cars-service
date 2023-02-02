4ee348432fa2164494465be0a56fa843cffb0886
NahrinMalakes <nahrinmalakes12@gmail.com>
2023-02-02 16:37:25 +0200

    removed junk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import styles from "./index.module.css";
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
 
 import { api } from "../utils/api";
 
