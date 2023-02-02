549405cdf359fe6d5dee44fd22c45e7e8ccf625b
NahrinMalakes <nahrinmalakes12@gmail.com>
2023-02-02 16:50:34 +0200

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-<<<<<<< HEAD
-=======
 import { ChakraProvider } from "@chakra-ui/react";
->>>>>>> cefc5d7 (.)
 
 import { api } from "../utils/api";
 
